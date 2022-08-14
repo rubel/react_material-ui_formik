8d3cf42e979dbf9927df9654e5a7b2501d8e452b
rubel <rubel_xpress@yahoo.com>
2022-08-14 22:38:33 +0600

    initial commit

---
+++ react_material_formik/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BasicMuiFormWithFormik from './BasicMuiFormWithFormik';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicMuiFormWithFormik />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_material_formik/src/BasicMuiFormWithFormik.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Form, Field, Formik } from "formik";
+import { Autocomplete } from "formik-mui";
+import { TextField } from "formik-mui";
+import MuiTextField from "@mui/material/TextField";
+import { Box } from "@mui/material";
+
+function BasicMuiFormWithFormik() {
+    return (
+        <Formik
+          enableReinitialize
+          initialValues={{ movie: "", year: null }}
+          onSubmit={(values) => console.log(values)}
+          validateOnMount
+        >   
+          {({ isValid, values, setFieldValue }) => {
+            return (
+              <Form>
+                <Field
+                  name="name"
+                  component={Autocomplete}
+                  options={[
+                    { title: "The Godfather", year: 1972 },
+                    { title: "The Godfather: Part II", year: 1974 },
+                    { title: "The Dark Knight", year: 2008 },
+                    { title: "12 Angry Men", year: 1957 },
+                    { title: "Schindler's List", year: 1993 },
+                    { title: "The Shawshank Redemption", year: 1994 }
+                  ]}
+                  getOptionLabel={(option) => option.title}
+                  style={{ width: 300 }}
+                  onChange={(event, value) => {
+                    setFieldValue("year", value ? value : "");
+                    setFieldValue("releaseYear", value ? value.year : "");
+                  }}
+                  renderInput={(params) => (
+                    <MuiTextField
+                      {...params}
+                      name="autocomplete"
+                      label="Movie"
+                      variant="outlined"
+                    />
+                  )}
+                />
+    
+                <Box marginBottom={3} />
+                <Field
+                  name="year"
+                  component={Autocomplete}
+                  options={[
+                    { title: "The Godfather", year: 1972 },
+                    { title: "The Godfather: Part II", year: 1974 },
+                    { title: "The Dark Knight", year: 2008 },
+                    { title: "12 Angry Men", year: 1957 },
+                    { title: "Schindler's List", year: 1993 },
+                    { title: "The Shawshank Redemption", year: 1994 }
+                  ]}
+                  getOptionLabel={(option) => option.year}
+                  style={{ width: 300 }}
+                  renderInput={(params) => (
+                    <MuiTextField
+                      {...params}
+                      name="autocomplete"
+                      label="Release Year"
+                      variant="outlined"
+                    />
+                  )}
+                />
+                <Box marginBottom={3} />
+                <Field
+                  component={TextField}
+                  placeholder="Release year"
+                  name="releaseYear"
+                  variant="outlined"
+                  InputProps={{ notched: true }}
+                />
+              </Form>
+            );
+          }}
+        </Formik>
+      );
+}
+
+export default BasicMuiFormWithFormik;
