50de1d1155b8b7782b5c759e8111d7165ffa582a
rubel <rubel_xpress@yahoo.com>
2022-08-14 22:47:11 +0600

    initial commit

--- react_material_formik/src/BasicMuiFormWithFormik.jsx
+++ react_material_formik/src/BasicMuiFormWithFormik.jsx
@@ -20,12 +20,10 @@ function BasicMuiFormWithFormik() {
                   name="name"
                   component={Autocomplete}
                   options={[
-                    { title: "The Godfather", year: 1972 },
-                    { title: "The Godfather: Part II", year: 1974 },
-                    { title: "The Dark Knight", year: 2008 },
-                    { title: "12 Angry Men", year: 1957 },
-                    { title: "Schindler's List", year: 1993 },
-                    { title: "The Shawshank Redemption", year: 1994 }
+                    { title: "Java", year: 1973 },
+                    { title: "PHP", year: 1974 },
+                    { title: "Python", year: 2008 },
+                    { title: "ActionScript", year: 1957 },
                   ]}
                   getOptionLabel={(option) => option.title}
                   style={{ width: 300 }}
