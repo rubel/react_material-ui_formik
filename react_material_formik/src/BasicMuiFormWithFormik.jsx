1ea86d6a9368683dd7f94dbd03ca10c604fd2bdb
rubel <rubel_xpress@yahoo.com>
2022-08-15 16:38:13 +0600

    with necessary components

--- react_material_formik/src/BasicMuiFormWithFormik.jsx
+++ react_material_formik/src/BasicMuiFormWithFormik.jsx
@@ -1,81 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Field, Formik } from "formik";
-import { Autocomplete } from "formik-mui";
-import { TextField } from "formik-mui";
+import { TextField,Autocomplete, CheckboxWithLabel, Switch } from "formik-mui";
+import { Alert, Box, Button, Collapse, Snackbar } from "@mui/material";
 import MuiTextField from "@mui/material/TextField";
-import { Box } from "@mui/material";
 
 function BasicMuiFormWithFormik() {
+  const [snackBarOpen, setSnakeBarOpen] = useState(false);
+    const [alertShown, setAlertShown] = useState(false);
+
+    const religionList = ["Islam","Christanity","Hinduism"]
+
     return (
-        <Formik
-          enableReinitialize
-          initialValues={{ movie: "", year: null }}
-          onSubmit={(values) => console.log(values)}
-          validateOnMount
-        >   
-          {({ isValid, values, setFieldValue }) => {
-            return (
-              <Form>
-                <Field
-                  name="name"
-                  component={Autocomplete}
-                  options={[
-                    { title: "Java", year: 1973 },
-                    { title: "PHP", year: 1974 },
-                    { title: "Python", year: 2008 },
-                    { title: "ActionScript", year: 197 },
-                  ]}
-                  getOptionLabel={(option) => option.title}
-                  style={{ width: 300 }}
-                  onChange={(event, value) => {
-                    setFieldValue("year", value ? value : "");
-                    setFieldValue("releaseYear", value ? value.year : "");
+      <div>
+        <div className="fullShadow">
+          <div className="customerFormBg">
+              <Button onClick={()=>{setSnakeBarOpen(true);}}>show snackbar</Button>
+              <Button onClick={()=>{setAlertShown(true);}}>show alert</Button>
+
+              <Snackbar
+                  open={snackBarOpen}
+                  autoHideDuration={3000}
+                  onClose={()=>{
+                    setSnakeBarOpen(false);
                   }}
-                  renderInput={(params) => (
-                    <MuiTextField
-                      {...params}
-                      name="autocomplete"
-                      label="Movie"
-                      variant="outlined"
-                    />
-                  )}
-                />
-    
-                <Box marginBottom={3} />
-                <Field
-                  name="year"
-                  component={Autocomplete}
-                  options={[
-                    { title: "The Godfather", year: 1972 },
-                    { title: "The Godfather: Part II", year: 1974 },
-                    { title: "The Dark Knight", year: 2008 },
-                    { title: "12 Angry Men", year: 1957 },
-                    { title: "Schindler's List", year: 1993 },
-                    { title: "The Shawshank Redemption", year: 1994 }
-                  ]}
-                  getOptionLabel={(option) => option.year}
-                  style={{ width: 300 }}
-                  renderInput={(params) => (
-                    <MuiTextField
-                      {...params}
-                      name="autocomplete"
-                      label="Release Year"
-                      variant="outlined"
-                    />
-                  )}
-                />
-                <Box marginBottom={3} />
-                <Field
-                  component={TextField}
-                  placeholder="Release year"
-                  name="releaseYear"
-                  variant="outlined"
-                  InputProps={{ notched: true }}
-                />
-              </Form>
-            );
-          }}
-        </Formik>
+                  message="Message Here"
+              />
+
+              {/* severity="error"
+              severity="warning"
+              severity="info"
+              severity="success" */}
+              <Collapse in={alertShown}>
+                  <Alert onClose={() => {setAlertShown(false)}}>This is a success alert — check it out!</Alert>
+              </Collapse>
+
+              <Formik
+                enableReinitialize
+                initialValues={{ name:"",password:"",releaseYear:"", address:"",loveIt:"",switch:"",religion:"" }}
+                onSubmit={(values) => {
+                  alert(JSON.stringify(values, null, 2));
+                }}
+                validateOnMount
+              >   
+                    <Form>
+                      <Field
+                        component={TextField}
+                        type="text"
+                        label="Name"
+                        name="name"
+                      />
+
+                      <Box marginBottom={3} />
+                      <Field
+                        component={TextField}
+                        type="password"
+                        label="Password"
+                        name="password"
+                      />
+                      
+                      <Box marginBottom={3} />
+                      
+                      <Field
+                        component={TextField}
+                        placeholder="Release year"
+                        name="releaseYear"
+                        variant="outlined"
+                        InputProps={{ notched: true }}
+                      />
+                      <Box marginBottom={3} />
+
+                      <Field id="address"  name="address" style={{ width: "100%",height:"120px" }}  component="textarea" />
+
+                      <Field
+                        component={CheckboxWithLabel}
+                        type="checkbox"
+                        name="loveIt"
+                        Label={{ label: 'Love It?' }}
+                      />
+                      <Field component={Switch} type="checkbox" name="switch" />
+
+                      <Box height={15} />
+
+                      <Field
+                        name="religion"
+                        component={Autocomplete}
+                        options={religionList}
+                        style={{ width: "100%" }}
+                        renderInput={(params) => (
+                          <MuiTextField
+                            {...params}
+                            name="religion"
+                            label="Religion"
+                            variant="outlined"
+                          />
+                        )}
+                      />
+                      
+                      <Box height={30} />
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                      >
+                        Submit
+                      </Button>
+                    </Form>
+
+              </Formik>
+              </div>
+            </div>
+        </div>
       );
 }
 
